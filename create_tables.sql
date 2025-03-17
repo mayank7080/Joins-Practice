9c3766703cec0e58cb80bba2c553a6766940cb51
Mayank Kumar <mayankpandey7080@gmail.com>
2025-03-17 22:01:58 +0530

    Create create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employees (
+    emp_id INT PRIMARY KEY,
+    emp_name VARCHAR(50),
+    dept_id INT
+);
+
+CREATE TABLE departments (
+    dept_id INT PRIMARY KEY,
+    dept_name VARCHAR(50)
+);
