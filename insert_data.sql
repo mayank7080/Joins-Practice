80e2e83e1a3ebf67e9891cf954e0a244e62d48ce
Mayank Kumar <mayankpandey7080@gmail.com>
2025-03-17 22:02:55 +0530

    Create insert_data.sql

---
+++ insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO employees (emp_id, emp_name, dept_id) VALUES 
+(1, 'Alice', 101),
+(2, 'Bob', 102),
+(3, 'Charlie', NULL),
+(4, 'David', 104);
+
+INSERT INTO departments (dept_id, dept_name) VALUES 
+(101, 'HR'),
+(102, 'IT'),
+(103, 'Finance'),
+(104, 'Marketing');
